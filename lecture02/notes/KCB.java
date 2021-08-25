ab11e88e49a708ed8f320624ae0c761e4ec5718a
MeoWoo6 <meowoo666@gmail.com>
2021-08-25 15:54:20 +0200

    new lecture code

---
+++ lecture02/notes/KCB.java
@@ -0,0 +1,15 @@
+package notes;
+
+import javax.swing.*;
+
+public class KCB {
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        JTable table = new JTable(new KCBData());
+        JScrollPane pane = new JScrollPane(table);
+        frame.add(pane);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
---
+++ lecture02/notes/KCBData.java
@@ -0,0 +1,61 @@
+package notes;
+
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+
+public class KCBData implements TableModel {
+    private String[] title = {"周一", "周二", "周三", "周四", "周五", "周六", "周日"};
+    private String[][] data = new String[8][7];
+
+    public KCBData(){
+        for(int i = 0; i < data.length; i++){
+            for(int j = 0; j < data[0].length; j++){
+                data[i][j] = "";
+            }
+        }
+    }
+    @Override
+    public int getRowCount() {
+        return 8;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 7;
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        return title[columnIndex];
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        return String.class;
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return true;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return data[rowIndex][columnIndex];
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        data[rowIndex][columnIndex] = (String) aValue;
+    }
+
+    @Override
+    public void addTableModelListener(TableModelListener l) {
+
+    }
+
+    @Override
+    public void removeTableModelListener(TableModelListener l) {
+
+    }
+}
